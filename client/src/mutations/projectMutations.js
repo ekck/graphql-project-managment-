d61fdbfca34307d6fb2a943fe09847fd7f05d67b
kiprono <kiprono_emmanuel@yahoo.com>
2022-09-20 16:05:56 +0300

    delete mutations

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -25,5 +25,13 @@ const ADD_PROJECT = gql`
       }
     }
   }
+`; 
+
+const DELETE_PROJECT = gql`
+mutation DeleteProject($id: ID!){
+  deleteProject(id: $id) {
+    id
+  }
+}
 `;
-export { ADD_PROJECT }
+export { ADD_PROJECT, DELETE_PROJECT }
